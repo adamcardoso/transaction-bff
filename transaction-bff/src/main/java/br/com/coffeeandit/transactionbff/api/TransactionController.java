73e26b3d5d9656db242fdd6d9d263c6cd1e41720
Adam Cardoso <adamjuniorcardoso@gmail.com>
2023-10-19 14:17:58 -0300

    feat: criando RequestTransactionDTO, TransactionController, TransactionDTO

---
+++ transaction-bff/src/main/java/br/com/coffeeandit/transactionbff/api/TransactionController.java
@@ -0,0 +1,33 @@
+package br.com.coffeeandit.transactionbff.api;
+
+import br.com.coffeeandit.transactionbff.dto.RequestTransactionDTO;
+import br.com.coffeeandit.transactionbff.dto.TransactionDTO;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<TransactionDTO> enviarTransacao(@RequestBody final RequestTransactionDTO requestTransactionDTO){
+        return Mono.empty();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<TransactionDTO> buscarTransacao(@PathVariable("id") final String uuid){
+        return Mono.empty();
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<TransactionDTO> removerTransacao(@PathVariable("id") final String uuid){
+        return Mono.empty();
+    }
+
+    @PatchMapping(value = "/{id}/confirmar")
+    public Mono<TransactionDTO> confirmarTransacao(@PathVariable("id") final String uuid){
+        return Mono.empty();
+    }
+}
